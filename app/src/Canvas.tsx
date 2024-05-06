a2f3cb10b832ffcc700a0096f0530d41dc9d3a8e
Assaf Basri <Faskoman90@gmail.com>
2024-05-06 19:34:05 +0300

    class enemy

--- app/src/Canvas.tsx
+++ app/src/Canvas.tsx
@@ -15,18 +15,37 @@ function Canvas() {
       animate();
     };
 
-    let x = 200;
+    map.src = "/src/assets/gameMapZoomed.png";
+
+    class Enemy {
+      position: { x: number; y: number };
+      width: number;
+      height: number;
+      constructor({ position = { x: 0, y: 0 }}) {
+        this.position = position;
+        this.width = 50;
+        this.height = 50;
+      }
+
+      draw() {
+        ctx!.fillStyle = "red";
+        ctx!.fillRect(this.position.x, this.position.y, this.width, this.height);
+      }
+
+      update() {
+        this.draw()
+        this.position.x += 1
+      }
+    }
+
+    const enemy = new Enemy( { position: {x: 200, y: 200}});
+
     function animate() {
       requestAnimationFrame(animate);
 
       ctx!.drawImage(map, 0, 0);
-
-      ctx!.fillStyle = "red"
-      ctx!.fillRect(x, 200, 50, 50);
-      x++;
+      enemy.update();
     }
-
-    map.src = "/src/assets/gameMapZoomed.png";
   }, []);
 
   return <canvas ref={canvasRef} width={1200} height={960}></canvas>;

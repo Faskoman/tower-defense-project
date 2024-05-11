803008815abb2e253e3271bb4d787f0a6f4d676d
Assaf Basri <Faskoman90@gmail.com>
2024-05-11 21:40:50 +0300

    refactored Bee and Flower components

---
+++ app/src/Flower.tsx
@@ -0,0 +1,21 @@
+import "./Flower.scss"
+
+function Flower() {
+    return (
+      <div className="flower">
+        <div className="flower__bud">
+          <div className="flower__bud__head"></div>
+          <div className="flower__bud__petals"></div>
+          <div className="flower__bud__petals"></div>
+          <div className="flower__bud__petals"></div>
+        </div>
+        <div className="flower__stem"></div>
+        <div className="flower__leaves">
+          <div className="flower__leaves__leaf"></div>
+          <div className="flower__leaves__leaf"></div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Flower;
--- app/src/MainMenu.tsx
+++ app/src/MainMenu.tsx
@@ -1,45 +1,17 @@
+import Bee from "./Bee";
+import Flower from "./Flower";
 import "./MainMenu.scss";
 
 function MainMenu() {
   return (
     <>
       <div className="container">
-        <div className="centered-flex">
-          <div className="bee">
-            <div className="bee__body">
-              <div>
-                <div className="bee__body__stripes"></div>
-                <div className="bee__body__stripes"></div>
-                <div className="bee__body__stripes"></div>
-              </div>
-            </div>
-            <div>
-              <div className="bee__eyes --right-eye"></div>
-              <div className="bee__eyes --left-eye"></div>
-            </div>
-            <div className="bee__wings">
-              <div className="bee__wings__wing"></div>
-              <div className="bee__wings__wing"></div>
-            </div>
-            <div>
-              <div className="bee__antena"></div>
-              <div className="bee__antena"></div>
-            </div>
-          </div>
+        <div className="background-bee">
+          <Bee />
+          <Bee />
         </div>
-        <div className="flower">
-          <div className="flower__bud">
-            <div className="flower__bud__head"></div>
-            <div className="flower__bud__petals"></div>
-            <div className="flower__bud__petals"></div>
-            <div className="flower__bud__petals"></div>
-          </div>
-          <div className="flower__stem"></div>
-          <div className="flower__leaves">
-            <div className="flower__leaves__leaf"></div>
-            <div className="flower__leaves__leaf"></div>
-          </div>
-        </div>
+        <Bee />
+        <Flower />
       </div>
     </>
   );
